1c6d4e9e696156a68d346a56730d1f7bf9865714
Сергей Чернышов <thesaren117@yandex.ru>
2020-05-26 14:17:37 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from application.salary import calculate_salary
+from application.db.people import get_employees
+import datetime
+
+
+
+if __name__=='__main__':
+    calculate_salary()
+    get_employees()
+
+    d = datetime.datetime.today()
+    print(f'Дата запуска программы:{d}')
